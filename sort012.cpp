a1ba6e71fbb43d6e88a57ff8a072af5d9c98539a
Venumadhav04 <72922816+Venumadhav04@users.noreply.github.com>
2023-01-10 18:50:25 +0530

    Create sort012.cpp
    very intuitive method and it is bit difficult to get this idea

---
+++ sort012.cpp
@@ -0,0 +1,27 @@
+
+void sort012(int a[], int n)
+{
+   int lo = 0;
+   int mid = 0;
+   int hi = n-1;
+   
+   while (mid <= hi) { 
+            switch (a[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(a[lo++], a[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(a[mid], a[hi--]); 
+                break; 
+            }
+        }
+}
