0a1a9a26f22b34570da3bdd899c290c1de711b98
Venumadhav04 <72922816+Venumadhav04@users.noreply.github.com>
2023-01-31 20:45:16 +0530

    Update factorial.cpp
    very good problem which would brush up our basics got struck in the middle. Must remember that the carry with more than one digit should be allocated in different array elements.

--- factorial.cpp
+++ factorial.cpp
@@ -1,47 +1,20 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-
- // } Driver Code Ends
-//User function template for C++
-    vector<int> factorial(int N){
+vector<int> factorial(int N){
         // code here
         vector<int> ans;
         ans.push_back(1);
-        int ans_size = 1;
+        
         int carry = 0;
         for(int i = 1;i <= N;i++){
-            ans_size = ans.size();
-            for(int j = 0;j < ans_size;j++){
-                ans[j] = ans[j]*i+carry;
-                carry = 0;
-                if(ans[j] > 9){
-                    if(j == ans.size()-1){
-                        ans.push_back(ans[j]/10);
-                        ans[j] = ans[j]%10;
-                    }else{
-                        
-                        carry = ans[j]/10;
-                        ans[j] = ans[j]%10;
-                    }
-                }
-            
+            for(int j = 0;j < ans.size();j++){
+                ans[j] = ans[j]*i + carry;
+                carry = ans[j]/10;
+                ans[j] = ans[j]%10;
+            }
+            while(carry){
+                ans.push_back(carry%10);
+                carry = carry/10;
             }
         }
         reverse(ans.begin(), ans.end());
         return ans;
-    
     }
-
-
-
-int main() {
-   
-        vector<int> result = factorial(10);
-        for (int i = 0; i < result.size(); ++i){
-            cout<< result[i];
-        }
-        cout << endl;
-
-    return 0;
-} 
