61a95a25ca1aa32c3b0793637d73adb847c9848e
Venumadhav04 <72922816+Venumadhav04@users.noreply.github.com>
2023-02-14 20:45:00 +0530

    Create solveWordWrap.cpp
    it is basically recursive solution integrated with dp, the three conditions that were mentioned, if there is no enough space, when there is enough space and then taking the minimum of both the conditions.

---
+++ solveWordWrap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[505][2005];
+    int rec(int i, int rem, vector<int> nums, int k) {
+        if(i == nums.size()){
+            return 0;
+        }
+        if(dp[i][rem] != -1) return dp[i][rem];
+        
+        if(rem<nums[i]){
+            dp[i][rem]  = (rem+1)*(rem+1) + rec(i+1, k-nums[i]-1, nums, k);
+        }else {
+            int choice1 = (rem+1)*(rem+1) + rec(i+1, k-nums[i]-1, nums, k);
+            int choice2 = rec(i+1, rem-nums[i]-1, nums, k);
+            dp[i][rem]  = min(choice1, choice2);
+        }
+        
+        return dp[i][rem];
+    }
+    int solveWordWrap(vector<int>nums, int k) 
+    { 
+        // Code here
+        memset(dp, -1,sizeof(dp));
+        return rec(0, k, nums, k);
+    } 
+};
