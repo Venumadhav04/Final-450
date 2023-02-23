55f9119618ac83655aa72a33d16971af2ad3d25a
Venumadhav04 <72922816+Venumadhav04@users.noreply.github.com>
2023-02-24 00:31:06 +0530

    Update lps.cpp

--- lps.cpp
+++ lps.cpp
@@ -1,14 +1,15 @@
-int lps(String s) {
-        // code here
+class Solution{
+  public:		
+	int lps(string s) {
+	    // Your code goes here
+	    int n = s.length();
         
-        int n = s.length();
-        
-        int[] lps = new int[n];
+        int lps[n];
         int i = 0, j =1;
         lps[0] = 0;
         
-        while(j less than n){
-            if(s.charAt(i) == s.charAt(j)){
+        while(j < n){
+            if(s[i] == s[j]){
                 lps[j] = i + 1 ;
                 i++;
                 j++;
@@ -24,20 +25,5 @@ int lps(String s) {
         }
         
         return lps[n-1];
-        
-        //s.sustring(0, lps[n-1]);
-        
-        
-       /* 
-        
-        if(lps[n-1] greater n/2){
-            // return n/2;
-            //return s.sustring(0, n/2);
-        }else{
-            // return lps[n-1];
-            // s.sustring(0, lps[n-1]);
-        }
-        
-        */
-        
-    }
+	}
+};
